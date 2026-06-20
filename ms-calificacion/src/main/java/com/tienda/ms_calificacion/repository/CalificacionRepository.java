ef8a663ff5b41d32c7134d5f5b394e528f98cc73
nacho <ignac.alvarezs@duocuc.cl>
2026-06-20 02:46:08 -0400

    intentos de ganarle al error

--- ms-calificacion/src/main/java/com/tienda/ms_calificacion/repository/CalificacionRepository.java
+++ ms-calificacion/src/main/java/com/tienda/ms_calificacion/repository/CalificacionRepository.java
@@ -1,6 +1,9 @@
 package com.tienda.ms_calificacion.repository;
 
-@Repository
-public interface CalificacionRepository extends JpaRepository<Calificacion, Long> {
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.tienda.ms_calificacion.model.calificacion;
 
+@Repository
+public interface CalificacionRepository extends JpaRepository<alificacion, Long> {
+}
--- ms-calificacion/src/main/java/com/tienda/ms_calificacion/repository/EstadolocalRepository.java
+++ ms-calificacion/src/main/java/com/tienda/ms_calificacion/repository/EstadolocalRepository.java
@@ -1,6 +1,9 @@
-package com.tienda.ms_calificacion.repository;         
+package com.tienda.ms_calificacion.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.tienda.ms_calificacion.model.estadolocal;
 
 @Repository
 public interface EstadolocalRepository extends JpaRepository<Estadolocal, Long> {
 }
-
